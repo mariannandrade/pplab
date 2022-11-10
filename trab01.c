eefb4ae06281297cc44aede27bb59b63db2e257f
mariannandrade <115123397+mariannandrade@users.noreply.github.com>
2022-11-09 22:30:52 -0300

    Add files via upload

---
+++ trab01.c
@@ -0,0 +1,12 @@
+/*
+* Arquivo: aritmInt.c
+* Data criação: 08/11
+* Autor: Marianna Andrade
+*/ 
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	printf("Bom dia, meu querido mestre!\n");
+	return 0;
+}
