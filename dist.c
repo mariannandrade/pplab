eca30dae5cfc2776c291710fc2b4414f0d14210b
Marianna Andrade <115123397+mariannandrade@users.noreply.github.com>
2022-11-27 15:34:36 -0300

    Add files via upload

--- dist.c
+++ dist.c
@@ -9,14 +9,16 @@
 #include <math.h>
 
 int main (int argc, char *argv[]){
-    int xA, yA, xB, yB, i = 0;
+    float xA, yA, xB, yB, i = 0.0;
     printf("digite as coordenadas do ponto A\n");
     scanf("%f %f", &xA, &yA);
     printf("digite as coordenadas do ponto B\n");
     scanf("%f %f", &xB, &yB);
 
-    int raiz = (sqrtf((pow((xA-xB),2))+(pow((yA-yB),2))));
-    printf("%f", raiz);
+    float X = xA - xB;
+    float Y = yA - yB;
 
+    float raiz = (sqrtf((pow(X,2))+(pow(Y,2))));
+    printf("%g", raiz);
     return 0;
 }
